e67df7619f8ec6bca57fa27ae397a95fbdb83118
Kepool32 <kukso.artem@mail.ru>
2024-12-08 17:00:19 +0300

    Added cors logic

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const config = require('./src/config/config');
 const connectDB = require('./src/config/db');
 const vacancyRoutes = require('./src/routes/vacancyRoutes');
 const requestLogger = require('./src/middleware/requestLogger');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(requestLogger);
 // Middleware для парсинга JSON
